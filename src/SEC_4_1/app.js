86fd35e834bafd1b49a7bc296b4239450b00787b
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2022-05-22 08:02:35 +0530

    SEC4.1

--- src/SEC_4_1/app.js
+++ src/SEC_4_1/app.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
-
-fs.readFile("src/SEC_4_1/test.txt", { encoding: "utf8" }, (err, context) => {
-  if (err) throw console.error(err);
-  console.log(context);
-});
+// import path from "path";
+const path = require("path");
+//Encodind Should be Specified other wise we get Buffred Data during File Operation
+fs.readFile(
+  path.resolve(__dirname, "test.txt"),
+  { encoding: "utf8" },
+  (err, context) => {
+    if (err) throw console.error(err);
+    console.log(context);
+  }
+);
